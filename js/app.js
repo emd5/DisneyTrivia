a6717ac6265a997f02aa179cdfd71265bc5f9c11
emd5 <emahoney5@mail.greenriver.edu>
2018-07-10 09:09:34 -0700

    added hike results

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ function showPage(n) {
     }
     if (n === (x.length - 1)) {
         x[n-1].style.display = "none";
-
+        results();
         document.getElementById("nextBtn").innerHTML = "Done";
     }
 }
@@ -37,3 +37,65 @@ function nextDisplay(n) {
     }
     showPage(currentFormPage)
 }
+
+function results(){
+    var terrain;
+    var name = document.getElementById("first").value;
+    // console.log(name);
+
+
+    var distance = document.getElementById("distanceOption");
+    var selectedDistance = distance.options[distance.selectedIndex].value
+    // console.log (selectedDistance);
+
+    var terrainElements  = document.getElementsByName("terrain");
+    for(var i=0; i< terrainElements.length; i++){
+        if(terrainElements[i].checked){
+            terrain = terrainElements[i];
+            // console.log("Terrain selected: "+ terrain);
+        }
+    }
+
+    var result = 'Thank you '+ name + '! The terrain you have selected:  '
+        + terrain.value + '. Distance selected: '
+        + selectedDistance;
+
+    // console.log(result);
+
+    hikeResults(terrain,selectedDistance);
+
+    return  document.getElementById("results").innerHTML = result;
+
+}
+
+function hikeResults(terrain, selectedDistance){
+        var hikeResults = document.getElementById("hikeResultDisplay");
+
+        if(terrain.value === "waterfall" && selectedDistance === "2 mile or less") {
+            var textResult = "Snoqualmie Falls |Terrain: waterfall |  Distance: 1.4 miles, roundtrip";
+
+        }else if(terrain.value === "waterfall" && selectedDistance === "2-5 miles") {
+            var textResult = "Franklin Falls |Terrain: waterfall |  Distance: 2.0 miles, roundtrip";
+
+        }else if(terrain.value === "waterfall" && selectedDistance === "5+ miles"){
+            var textResult = "Lewis River Falls |Terrain: waterfall |  Distance: 8.8 miles, roundtrip";
+
+        }else if(terrain.value === "mountain" && selectedDistance === "2-5 mile or less"){
+            var textResult = "I hike longer hikes, sorry!";
+
+        }else if(terrain.value === "mountain" && selectedDistance === "2-5 miles"){
+            var textResult = "Rattlesnake Ledge |Terrain: mountain |  Distance: 5.0 miles, roundtrip";
+
+        }else if(terrain.value === "mountain" && selectedDistance === "5+ miles"){
+            var textResult = "Burroughs Mountain [Sunrise at Mt. Rainier] |Terrain: " +
+                "mountain |  Distance: 7.4 miles, roundtrip";
+        }else{
+            var textResult = "Fremont Lookout [Sunrise at Mt. Rainier] |Terrain: mountain " +
+                "|  Distance: 5.6 miles, roundtrip";
+            return hikeResults.innerHTML = textResult;
+        }
+    return hikeResults.innerHTML = textResult;
+
+}
+
+
